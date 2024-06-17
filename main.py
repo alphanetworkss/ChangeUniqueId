cd9bbd3a089e6c1d638e863f47dbe0177dd092eb
alphanetworkss <160410546+alphanetworkss@users.noreply.github.com>
2024-06-17 07:58:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,191 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import random
-import time
-import threading
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+import pytz
+import datetime
+import os, argparse
+import asyncio, shlex
+from os.path import join
+from aiofiles.os import remove
+from aiohttp import ClientSession
+from typing import Union, List
+from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig
+from telegram import Bot
+
+__version__ = "1.0"
+__author__ = "https://t.me/hiddenextractorbot"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024"
+
+basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[FileHandler('logs.txt', encoding='utf-8'),
+              StreamHandler()],
+    level=INFO)
+
+LOGGER = getLogger("drm")
+
+os.makedirs("Videos/", exist_ok=True)
+
+def print_ascii_art():
+    text = '\033[92m'"""
+    ____  ____  __  ___   ____                      __                __
+   / __ \/ __ \/  |/  /  / __ \____ _      ______  / /___  ____ _____/ /
+  / / / / /_/ / /|_/ /  / / / / __ \ | /| / / __ \/ / __ \/ __ `/ __  / 
+ / /_/ / _, _/ /  / /  / /_/ / /_/ / |/ |/ / / / / / /_/ / /_/ / /_/ /  
+/_____/_/ |_/_/  /_/  /_____/\____/|__/|__/_/ /_/_/\____/\__,_/\__,_/   
+                                                                           
+    """'\033[0m''\033[33m'f'v{__version__} by {__author__}, {__license__} @{__copyright__}\n''\033[0m'
+    print(text)
+
+class SERVICE(ABC):
+
+    def __init__(self):
+        self._remoteapi = "https://app.magmail.eu.org/get_keys"
+
+    @staticmethod
+    def c_name(name: str) -> str:
+        for i in ["/", ":", "{", "}", "|"]:
+            name = name.replace(i, "_")
+        return name
+
+    def get_date(self) -> str:
+        tz = pytz.timezone('Asia/Kolkata')
+        ct = datetime.datetime.now(tz)
+        return ct.strftime("%d %b %Y - %I:%M%p")
+
+    async def get_keys(self):
+        async with ClientSession(headers={"user-agent": "okhttp"}) as session:
+            async with session.post(self._remoteapi,
+                                    json={"link": self.mpd_link}) as resp:
+                if resp.status != 200:
+                    LOGGER.error(f"Invalid request: {await resp.text()}")
+                    return None
+                response = await resp.json(content_type=None)
+        self.mpd_link = response["MPD"]
+        return response["KEY_STRING"]
 
 
-json_content = {
-    "type": "service_account",
-    "project_id": "alpha-424216",
-    "private_key_id": "d8615e606fc149c3f4f28c187e3f5f8ea414cbf2",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLJfsbTGKRhBIi\nYBLM1KOrEHXvtY1BkSrJTQANdkt7WdLWL2AoHTuhXNrlDRbnZM5Qpg4ucmj23kmO\ncI1uuXjiHr4xPC421gDJiwFbvpo0Yfg+GUn7d1XpoFKAeKfSCOTuEyHbMGPueCH+\nEu3M57c5YeErAi3s73nfy75lMuUh3gmHgLfWpMzGUG9bzHKXBq6J18EDzoBFWbXC\nB7/d7Uwf7KK2LMZX/23pKr3pedspTRUC3Ymlh3nPXGpJGzv2gTATZWHblNp0ikAu\nqSAfMdqOvFdE1jqHUcxc+9S1dmztPilCt1sevDOOdCQCPhdPMUWbyFdruZCFRcpX\n0ok7MCuzAgMBAAECggEAXDwQ4CqUwEPqJeBf+M78xpDxMxZHncLm3cHPqc0RZV55\nixS0gFIRfmiV3BRO112eDODwJGd+v8NaO0lf9atRNr1Flj4gL3aHofoycjC+e9UV\nbEB4Jive/nVzr+/YJlUyd+OI5id43898g4qdnICCYJPPs4LITkDGoXU0q2bpwK9q\ndAbMG5dn/wR0aNFUSbcKpY9AfpgeVUw+ii7MnGxbOcBpvOfeyZm8yZArIXSl5xtQ\nfqNKHzrnEBFqQPOmARL6K75Y2CbqNE9RjxR/zyHecWEB0oRlr9fyd+3VoShfzqIj\nIMeIBNT0FloW890/o6nunt3iobgWnMN1EHn7e1tcZQKBgQD9/ZIZvkUVG5A9TISr\npJtP+DxThxt3tRZfeMknCeYF2NEt7JHYVFi1DB+1gtxbb1G82Mw6XwvdC3hqWUGk\nD0tvvZ8viYXLJFS7/le6e9LWG83xp1nqtEJitOmWY81dSKL1UsyImusTlOZRwH/9\njCKs8ksWjgS46Hi8GvkBEevgVQKBgQDMwW9jeOVZjNmScjSCQJeCtx56svOlYAOj\ndJTce95/R2yki/2a+VgXgwEJygr82COsFTKPTVKIXMP6r4bwvZCrpFChMF2QMOsI\nee236FSh4rvH9gWWM/Ji5Bcz6/nmQYrUCQQ3luUxciAjOWrBMchnEtElSWpZx5pC\nDXPybZjD5wKBgFwTJnmqnkSOn3V5XccTfzI4XTYyjmSRAzFNCi7aycZo2Tv6vMxd\nl2LaqEwWymdjLZB2T2MBvb0+QULcZI1i9gfmB+Ulx5ji7MzR4V+7L61qmvf1oQUS\nn9ZEm0FnoLUmddzVTUxgTMPmgLA2Rq/Vs2Ra2ZmtlkyqCcq3RHl807OZAoGBAJTD\nKWmtCX0hf2YOAaZsxWecIdHluG17r8kPR6RVftccjouShiQVavJSJOv21jrT0j8R\ne5VwCTm3pE+7PaAlKjn6fPZPjHrZf7m91H/clbi0YdgrH+38Oeutako39W1cS0A4\neM7mnAhrsXvGGJDa2Y5BtqCPkWw/QA0jdw04oVgbAoGAFbBkLgl/qCEHcWxDfhy8\n0yzR2k8IVZJiTYalIvXJzHVN/mv6a6B+Xzmswboh62Lnisam2INUKUZOSuIrodmv\n2xArzcHn/H6WFhTi+IJ0TKAlalalY0waLZQxtY5sx9RsobhTFimiClTrpE4zCFuB\n55pP+aML9WCUEpnTgFKTdvc=\n-----END PRIVATE KEY-----\n",
-    "client_email": "alphabot@alpha-424216.iam.gserviceaccount.com",
-    "client_id": "100039873888133124879",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/alphabot%40alpha-424216.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-}
-# Google Sheets API setup
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
-         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+class Download(SERVICE):
 
-# Replace 'path/to/credentials.json' with your actual credentials file path
-creds = ServiceAccountCredentials.from_json_keyfile_dict(json_content, scope)
-client = gspread.authorize(creds)
+    def __init__(self, name: str, resl: str, mpd: str):
+        super().__init__()
+        self.mpd_link = mpd
+        self.name = self.c_name(name)
+        self.vid_format = f'bestvideo.{resl}/bestvideo.2/bestvideo'
 
-# Open the Google Sheet by name
-sheet = client.open('PenPencil Data').sheet1
+        videos_dir = "Videos"
+        encrypted_basename = f"{self.name}_enc"
+        decrypted_basename = f"{self.name}_dec"
 
-def generate_unique_id(existing_ids):
-    """Generate a unique ID starting with 'ALPHA' followed by a 10-digit number."""
-    while True:
-        unique_number = 'ALPHA' + ''.join(random.choices('0123456789', k=10))
-        if unique_number not in existing_ids:
-            return unique_number
+        self.encrypted_video = join(videos_dir, f"{encrypted_basename}.mp4")
+        self.encrypted_audio = join(videos_dir, f"{encrypted_basename}.m4a")
+        self.decrypted_video = join(videos_dir, f"{decrypted_basename}.mp4")
+        self.decrypted_audio = join(videos_dir, f"{decrypted_basename}.m4a")
+        self.merged = join(videos_dir, f"{self.name} - {self.get_date()}.mkv")
 
-def check_and_update_sheet():
-    rows = sheet.get_all_values()
-    if len(rows) < 1:
-        print("No data in sheet.")
-        return
+    async def process_video(self):
+        key = await self.get_keys()
+        if not key:
+            LOGGER.error("Could not retrieve decryption keys.")
+            return
+        LOGGER.info(f"MPD: {self.mpd_link}")
+        LOGGER.info(f"Got the Keys > {key}")
+        LOGGER.info(f"Downloading Started...")
+        if await self.__yt_dlp_drm() and await self.__decrypt(
+                key) and await self.__merge():
+            LOGGER.info(f"Cleaning up files for: {self.name}")
+            await self.__cleanup_files()
+            LOGGER.info(f"Downloading complete for: {self.name}")
+            return self.merged
+        LOGGER.error(f"Processing failed for: {self.name}")
+        return None
 
-    headers = rows[0]
-    if 'Access_Token' not in headers or 'Batch_Id' not in headers or 'Batch Name' not in headers or 'Unique Id' not in headers:
-        print("Required columns are missing.")
-        return
+    async def __subprocess_call(self, cmd: Union[str, List[str]]):
+        if isinstance(cmd, str):
+            cmd = shlex.split(cmd)
+        process = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await process.communicate()
+        if process.returncode != 0:
+            LOGGER.error(
+                f"Command failed: {' '.join(cmd)}\nError: {stderr.decode()}")
+            return False
+        return True
 
-    access_token_idx = headers.index('Access_Token')
-    batch_id_idx = headers.index('Batch_Id')
-    batch_name_idx = headers.index('Batch Name')
-    unique_id_idx = headers.index('Unique Id')
+    async def __yt_dlp_drm(self) -> bool:
+        video_download = self.__subprocess_call(
+            f'yt-dlp -k --allow-unplayable-formats -f "{self.vid_format}" --fixup never "{self.mpd_link}" --external-downloader aria2c --external-downloader-args "-x 16 -s 16 -k 1M" -o "{self.encrypted_video}"'
+        )
+        audio_download = self.__subprocess_call(
+            f'yt-dlp -k --allow-unplayable-formats -f ba --fixup never "{self.mpd_link}" --external-downloader aria2c --external-downloader-args "-x 16 -s 16 -k 1M" -o "{self.encrypted_audio}"'
+        )
+        return await asyncio.gather(video_download, audio_download)
 
-    existing_ids = {row[unique_id_idx] for row in rows[1:] if len(row) > unique_id_idx and row[unique_id_idx]}
+    async def __decrypt(self, key: str):
+        LOGGER.info("Decrypting...")
+        video_decrypt = self.__subprocess_call(
+            f'mp4decrypt --show-progress {key} "{self.encrypted_video}" "{self.decrypted_video}"'
+        )
+        audio_decrypt = self.__subprocess_call(
+            f'mp4decrypt --show-progress {key} "{self.encrypted_audio}" "{self.decrypted_audio}"'
+        )
+        return await asyncio.gather(video_decrypt, audio_decrypt)
 
-    for idx, row in enumerate(rows[1:], start=2):
-        if len(row) > max(access_token_idx, batch_id_idx, batch_name_idx):
-            access_token = row[access_token_idx]
-            batch_id = row[batch_id_idx]
-            batch_name = row[batch_name_idx]
-            if access_token and batch_id and batch_name:
-                if len(row) <= unique_id_idx or not row[unique_id_idx]:  # Check if the Unique Id column is empty or not present
-                    unique_id = generate_unique_id(existing_ids)
-                    sheet.update_cell(idx, unique_id_idx + 1, unique_id)  # Update the Unique Id column
-                    existing_ids.add(unique_id)  # Add the new ID to the set of existing IDs
-                    print(f"Row {idx}: Generated and stored unique ID {unique_id}")
-                # else:
-                #     print(f"Row {idx}: Unique ID already exists")
+    async def __merge(self):
+        LOGGER.info("Merging...")
+        return await self.__subprocess_call(
+            f'ffmpeg -i "{self.decrypted_video}" -i "{self.decrypted_audio}" -c copy "{self.merged}"'
+        )
 
-def clear_unique_id_column():
-    while True:
-        try:
-            rows = sheet.get_all_values()
-            headers = rows[0]
-            if 'Unique Id' in headers:
-                unique_id_idx = headers.index('Unique Id')
-                for idx in range(2, len(rows) + 1):
-                    sheet.update_cell(idx, unique_id_idx + 1, '')
-                print("Cleared all Unique Ids")
-            else:
-                print("Unique Id column is missing.")
-        except Exception as e:
-            print(f"An error occurred while clearing Unique Id column: {e}")
-        time.sleep(6 * 60 * 60)  # Sleep for 6 hours
+    async def __cleanup_files(self):
+        for file_path in [
+                self.encrypted_video, self.encrypted_audio,
+                self.decrypted_audio, self.decrypted_video
+        ]:
+            try:
+                await remove(file_path)
+            except Exception as e:
+                LOGGER.warning(f"Failed to delete {file_path}: {str(e)}")
 
-def main_loop(interval=2):
-    while True:
-        try:
-            check_and_update_sheet()
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        time.sleep(interval)
+
+async def main(file_path: str, tg_bot_token: str, tg_channel_id: str, resl: str):
+    with open(file_path, 'r') as file:
+        mpd_links = [line.strip() for line in file.readlines()]
+
+    bot = Bot(token=tg_bot_token)
+
+    for index, mpd in enumerate(mpd_links):
+        name = f"video_{index + 1}"
+        downloader = Download(name, resl, mpd)
+        merged_file = await downloader.process_video()
+        if merged_file:
+            await bot.send_document(chat_id=tg_channel_id, document=open(merged_file, 'rb'))
+            LOGGER.info(f"Uploaded {merged_file} to Telegram channel.")
+
 
 if __name__ == "__main__":
-    # Start the thread to clear Unique Id column every 6 hours
-    clear_thread = threading.Thread(target=clear_unique_id_column)
-    clear_thread.daemon = True
-    clear_thread.start()
-
-    # Run the main loop
-    main_loop()
+    print_ascii_art()
+    parser = argparse.ArgumentParser(
+        description='Download and Decrypt DRM Video via Remote Key API and upload to Telegram channel')
+    parser.add_argument('-f',
+                        '--file',
+                        type=str,
+                        help='Path to the text file containing MPD links',
+                        required=True)
+    parser.add_argument('-r',
+                        '--resl',
+                        type=str,
+                        help='Video Resolution (1/2/3) where 1 is highest and 3 is lowest available resolution',
+                        default="1")
+    parser.add_argument('-t',
+                        '--token',
+                        type=str,
+                        help='Telegram Bot Token',
+                        required=True)
+    parser.add_argument('-c',
+                        '--channel
